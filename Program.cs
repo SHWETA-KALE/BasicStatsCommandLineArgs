60b0a24bb10b5258495961f7624be44178463a91
SHWETA-KALE <99083461+SHWETA-KALE@users.noreply.github.com>
2024-07-12 20:16:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CommandLineDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 5)
+            {
+                Console.WriteLine("Please provide 5 integers as command-line arguments.");
+                return;
+            }
+
+            int[] arr = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            
+                arr[i] = Convert.ToInt32(args[i]);
+            
+
+            int sum = 0;
+            float avg;
+            int min = arr[0];
+            int max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+
+                if (arr[i] < min)
+                
+                    min = arr[i];
+                
+
+                if (arr[i] > max)
+                
+                    max = arr[i];
+                
+            }
+
+            avg = (float)sum / arr.Length;
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Average: " + avg);
+            Console.WriteLine("Count: " + arr.Length);
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Max: " + max);
+
+            Console.ReadLine();
+        }
+    }
+}
